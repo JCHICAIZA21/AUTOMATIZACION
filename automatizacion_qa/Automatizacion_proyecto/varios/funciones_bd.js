cc6d0988845e1ee2d3de2215ade635170a0b5982
Juan Diego Chicaiza <juan.chicaiza@intempo.co>
2022-05-11 11:14:33 -0500

    v.1.0
    cambios v.1.0

--- automatizacion_qa/Automatizacion_proyecto/varios/funciones_bd.js
+++
@@ -1,7 +0,0 @@
-const APPROOT = require('app-root-path');
-
-const FUNCIONESBD = {
-    bd: { getNextSequence, comparendoexistente, parametro_falloManual, meses_caducidad, entidadAgente, infoComparendo, comparendoExistente, codigoInfraccionComparendo } = require(APPROOT.path + '/base_datos/contravencional.js'),
-}
-
-module.exports.FUNCIONESBD = FUNCIONESBD;
--- automatizacion_qa/README.md
+++
@@ -1,4 +0,0 @@
-
-automatizacion_qa
-
-prueba
